a27d417756980e0b4b97c1b7d665850a237f6626
Giuseppe Burtini <giuseppe@burtini.ca>
2025-04-13 10:26:47 -0700

    prepare build for npm

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  publicDir: false,
+  clean: true,
+  minify: true,
+  format: ["cjs"],
+  outDir: "build",
+});
